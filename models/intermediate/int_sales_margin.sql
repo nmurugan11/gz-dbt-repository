1ece633a7d62c1725da1e04e476762a5eef39597
nmuruganantham11@gmail.com <nmuruganantham11@gmail.com>
2024-04-17 10:40:14 +0000

    orders_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -9,4 +9,4 @@ SELECT
     s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
 FROM {{ref("stg_raw__sales")}} s 
 LEFT JOIN {{ref("stg_raw__product")}} p 
-    USING (products_id)
+    USING (products_id) 
