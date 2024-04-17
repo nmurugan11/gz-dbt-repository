e6daab7c5d0c338419d691907be1750d79521826
nmuruganantham11@gmail.com <nmuruganantham11@gmail.com>
2024-04-17 10:12:46 +0000

    Created purchase_cost and margin columns

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,9 +4,9 @@ SELECT
     orders_id,
     revenue,
     quantity,
-    CAST(p.purchase_price AS FLOAT64) AS purchase_price,
-    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin,
-    {{ margin_percent('s.revenue', 's.quantity*CAST(p.purchase_price AS FLOAT64)') }} AS margin_percent
+    CAST(purchase_price AS FLOAT64),
+    ROUND(s.quantity*cast(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
 FROM {{ref("stg_raw__sales")}} s 
 LEFT JOIN {{ref("stg_raw__product")}} p 
-USING (products_id)
+    USING (products_id)
