32a8bb09dee1c01b155f7ba65d9264386202fdbf
nmuruganantham11@gmail.com <nmuruganantham11@gmail.com>
2024-04-17 13:26:39 +0000

    finance_days.sql

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,4 +1,3 @@
-{{ config(schema='dbt_Nivetha') }} 
 SELECT
     date_date,
     COUNT(orders_id) AS nb_transactions,
@@ -15,15 +14,3 @@ SELECT
 FROM {{ref("int_orders_operational")}}
 GROUP BY date_date
 ORDER BY date_date DESC
-
-#Configuring models
-models:
-    greenweez_project:
-        intermediate:
-            materialized: view
-        mart:
-            materialized: table
-            finance:
-                schema: finance
-            supply chain:
-                schema: supply_chain
